4cf818447258b901687a9b8fa4e9133fb30039ff
Weedanta <baguswedanta17@gmail.com>
2025-04-01 12:32:46 +0700

    first commit

---
+++ TugasLogin/app/src/main/java/com/example/tugasteori/LoginActivity.java
@@ -0,0 +1,47 @@
+package com.example.tugasteori;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        EditText username = findViewById(R.id.username);
+        EditText password = findViewById(R.id.password);
+        Button login = findViewById(R.id.submit);
+
+        login.setOnClickListener(v -> {
+            String inputUsername = username.getText().toString();
+            String inputPassword = password.getText().toString();
+
+            // Validasi sederhana
+            if (inputUsername.equals("Hasbi") && inputPassword.equals("Hasbi")) {
+                Intent intent = new Intent(LoginActivity.this, List.class);
+                intent.putExtra("USERNAME", inputUsername);
+                startActivity(intent);
+            } else {
+                Toast.makeText(LoginActivity.this, "Username atau Password salah", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
