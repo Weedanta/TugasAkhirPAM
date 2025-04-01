d5b26f459661759d8352b4ca94ce491b64510a2a
Weedanta <baguswedanta17@gmail.com>
2025-04-01 13:30:21 +0700

    feat: configure colors

--- app/src/main/java/LoginAndSIgnup/LoginActivity.java
+++ app/src/main/java/LoginAndSIgnup/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.tugas_akhir_pam;
+package LoginAndSIgnup;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tugas_akhir_pam.R;
 
 public class LoginActivity extends AppCompatActivity {
 
